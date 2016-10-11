d774d776ea106b61f618bb0298aa9a783cc638ae
Everett Quebral <Everett.Quebral@gmail.com>
2016-10-11 16:15:24 -0700

    Adding an Event for Focuser State Changes like moving or ready

---
+++ FocuserStateChangedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASCOM.EQFocuser
+{
+    public class FocuserStateChangedEventArgs : EventArgs
+    {
+        public readonly bool IsMoving;
+        
+        public FocuserStateChangedEventArgs(bool isMoving)
+        {
+            this.IsMoving = isMoving;
+        }
+    }
+}
